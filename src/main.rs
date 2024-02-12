f6bccc7d597f3d1ee6d321bbda29c0989894e182
Juels, Titan <115128048+thjuels@users.noreply.github.com>
2024-02-12 16:54:17 +0800

    Finished Project

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 use std::env;
 
+use std::process;
+
+extern crate minigrep;
+
+use minigrep::Config;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    //let config= parse_config(&args); //assigns config using function
+    //using Config constructor
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        //changing to eprintln means errors don't output to output.txt
+        //when running 'cargo run > output.txt'
+        process::exit(1);
+    }
 }
+
+// fn parse_config(args: &[String]) -> Config {
+//     let query = args[1].clone();
+//     let filename = args[2].clone();
+//     Config { query, filename }
+// }
